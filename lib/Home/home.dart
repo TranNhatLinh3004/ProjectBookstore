c3cecc07c6de3f05390e3ff90690e162653418d7
thaicanhth <76417734+thaicanhth@users.noreply.github.com>
2022-12-14 01:55:17 +0700

    change 6 file

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -21,8 +21,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  // const MyApp({Key? key, }) : super(key: key);
-  // This widget is the root of your application.
+
   dynamic data;
   MyApp(this.data);
   @override
